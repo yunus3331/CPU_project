8b4ddc7b574dd4f823c38283fef2db650fce2f22
Davood Azhdari <davood.aj1384@gmail.com>
2026-01-30 20:23:41 +0330

    Creating files

---
+++ CPU.srcs/sim_1/new/cpu_tb.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 01/30/2026 08:11:53 PM
+-- Design Name: 
+-- Module Name: cpu_tb - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity cpu_tb is
+--  Port ( );
+end cpu_tb;
+
+architecture Behavioral of cpu_tb is
+
+begin
+
+
+end Behavioral;
