64debc2e3641bfaf84233f179033032463c2d63f
Davood Azhdari <davood.aj1384@gmail.com>
2026-02-04 15:25:26 +0330

    Edit D-memory and made it generic

--- CPU.srcs/sources_1/new/data_memory.vhd
+++ CPU.srcs/sources_1/new/data_memory.vhd
@@ -3,27 +3,45 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity data_memory is
+    generic (
+        DATA_WIDTH : integer := 8;
+        ADDR_WIDTH : integer := 8          -- 2^8 = 256
+    );
     port (
         clk        : in  std_logic;
         mem_we     : in  std_logic;
-        addr       : in  std_logic_vector(7 downto 0);
-        write_data : in  std_logic_vector(7 downto 0);
-        read_data  : out std_logic_vector(7 downto 0)
+        addr       : in  std_logic_vector(ADDR_WIDTH-1 downto 0);
+        write_data : in  std_logic_vector(DATA_WIDTH-1 downto 0);
+        read_data  : out std_logic_vector(DATA_WIDTH-1 downto 0)
     );
 end data_memory;
 
 architecture rtl of data_memory is
-    type ram_t is array (0 to 255) of std_logic_vector(7 downto 0);
-    signal ram : ram_t := (others => (others => '0'));
+
+    constant MEM_DEPTH : integer := 2**ADDR_WIDTH;
+
+    type mem_t is array (0 to MEM_DEPTH-1)
+        of std_logic_vector(DATA_WIDTH-1 downto 0);
+
+    signal mem : mem_t := (
+        0 => std_logic_vector(to_unsigned(5, DATA_WIDTH)),
+        1 => std_logic_vector(to_unsigned(3, DATA_WIDTH)),
+        others => (others => '0')
+    );
+
 begin
+
+    -- synchronous write
     process(clk)
     begin
         if rising_edge(clk) then
             if mem_we = '1' then
-                ram(to_integer(unsigned(addr))) <= write_data;
+                mem(to_integer(unsigned(addr))) <= write_data;
             end if;
         end if;
     end process;
 
-    read_data <= ram(to_integer(unsigned(addr)));
+    -- asynchronous read
+    read_data <= mem(to_integer(unsigned(addr)));
+
 end rtl;
