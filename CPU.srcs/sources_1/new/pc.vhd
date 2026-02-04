7e10fd95ee3c8dab0f80588c2c19b7c3ea9a7868
Davood Azhdari <davood.aj1384@gmail.com>
2026-02-04 15:26:12 +0330

    Debug pc register

--- CPU.srcs/sources_1/new/pc.vhd
+++ CPU.srcs/sources_1/new/pc.vhd
@@ -3,30 +3,30 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity pc is
+    generic (
+        ADDR_WIDTH : integer := 8
+    );
     port (
-        clk     : in  std_logic;
-        reset   : in  std_logic;
-        pc_en   : in  std_logic;
-        pc_next : in  std_logic_vector(7 downto 0);
-        pc_out  : out std_logic_vector(7 downto 0)
+        clk    : in  std_logic;
+        reset  : in  std_logic;
+        pc_out : out std_logic_vector(ADDR_WIDTH-1 downto 0)
     );
 end pc;
 
 architecture rtl of pc is
-    signal pc_reg : std_logic_vector(7 downto 0);
+    signal pc_reg : unsigned(ADDR_WIDTH-1 downto 0);
 begin
 
     process(clk, reset)
     begin
         if reset = '1' then
             pc_reg <= (others => '0');
+
         elsif rising_edge(clk) then
-            if pc_en = '1' then
-                pc_reg <= pc_next;
-            end if;
+            pc_reg <= pc_reg + 1;
         end if;
     end process;
 
-    pc_out <= pc_reg;
+    pc_out <= std_logic_vector(pc_reg);
 
 end rtl;
