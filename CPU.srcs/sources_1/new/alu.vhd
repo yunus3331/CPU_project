c9e91d4767024d2477ce018b43ccf6127b257728
Davood Azhdari <davood.aj1384@gmail.com>
2026-02-04 15:23:07 +0330

    Edit ALU and delete surplus operation like shift

--- CPU.srcs/sources_1/new/alu.vhd
+++ CPU.srcs/sources_1/new/alu.vhd
@@ -19,7 +19,7 @@ architecture Behavioral of alu is
     signal result_int : std_logic_vector(DATA_WIDTH-1 downto 0);
 begin
 
-    process(A, B, ALUcontrol)
+    process(A,B,ALUcontrol)
     begin
 
         result_int <= (others => '0');
@@ -37,26 +37,11 @@ begin
                 result_int <= A xor B;
             when "0101" =>
                 result_int <= not A;
-            when "0110" =>
-                result_int <= std_logic_vector(
-                                shift_left(unsigned(A), to_integer(unsigned(B)))
-                              );
-            when "0111" =>
-                result_int <= std_logic_vector(
-                                shift_right(unsigned(A), to_integer(unsigned(B)))
-                              );
-            when "1000" =>
-                if signed(A) < signed(B) then
-                    result_int <= (others => '1');
-                else
-                    result_int <= (others => '0');
-                end if;
             when others =>
                 result_int <= (others => '0');
         end case;
     end process;
 
-    Result <= result_int;
     Result <= result_int;
     Zero   <= '1' when unsigned(result_int) = 0 else '0';
 
